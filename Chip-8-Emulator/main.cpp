f2a3d551f7af0613447604fcdc8f1a8b13938eb7
Marco Mussini <marco.mussini75@gmail.com>
2020-06-16 08:46:36 +0100

    Integrated SDL2

--- Chip-8-Emulator/main.cpp
+++ Chip-8-Emulator/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include <SDL2/SDL.h>
 
 #include "Chip8.hpp"
 
@@ -22,6 +23,27 @@ int main(int argc, const char * argv[]) {
         std::cerr << "Program couldn't be loaded" << std::endl;
         exit(EXIT_FAILURE);
     }
-
+    
+    if (SDL_Init( SDL_INIT_VIDEO ) < 0) {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        exit(EXIT_FAILURE);
+    }
+    
+    SDL_Window *window = SDL_CreateWindow("Chip8 Emulator", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                          640, 480, SDL_WINDOW_SHOWN);
+    if (!window) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+    
+    SDL_Surface *screenSurface = SDL_GetWindowSurface( window );
+    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0x0, 0x0));
+    SDL_UpdateWindowSurface(window);
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    SDL_Delay(2000);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    
     return 0;
 }
